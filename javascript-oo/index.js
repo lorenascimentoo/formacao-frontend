e29c0872d9fdc822fc5b38ac87b8dc7e997ba3ab
Lorena Nascimento <lorenanascimento28@gmail.com>
2021-08-13 10:13:19 -0300

    Aula 2 - Curso de JAVASCRIPT: Programando a Orientação a Objetos

--- javascript-oo/index.js
+++ javascript-oo/index.js
@@ -1,20 +1,47 @@
+//Aplicando abstração
 class Cliente {
     nome;
     cpf;
+    }
+
+class ContaCorrente{
     agencia;
-    saldo;
+    // Aplicando encapsulamento
+     // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
+    _saldo=0; // convenção entre a comunidade js pra indicar que um atributo é privado
+
+    sacar(valor){ //método que recebe um parâmetro
+        if(this._saldo >= valor){
+            this._saldo -= valor;
+            return valor;
+        }
+    }
+
+    depositar(valor){
+        //Verifica se os valores são válidos para depósito
+        if(valor<=0){
+            return;
+        }
+        this._saldo += valor;
+    }
 }
 
 const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
 const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
-console.log(cliente1,cliente2);
+const ContaCorrenteRicardo = new ContaCorrente();
+ContaCorrenteRicardo.agencia = 1001;
+
+ContaCorrenteRicardo.depositar(-100);
+ContaCorrenteRicardo.depositar(100);
+ContaCorrenteRicardo.depositar(100);
+
+const valorSacado = ContaCorrenteRicardo.sacar(50);
+console.log(valorSacado);
+
+console.log(ContaCorrenteRicardo);
