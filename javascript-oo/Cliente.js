cb532a0d7d373f3109a20cfa12db16ff6c350fe8
Lorena Nascimento <lorenanascimento28@gmail.com>
2021-08-17 15:42:05 -0300

    Aula 5 - Curso de JAVASCRIPT: Programando a Orientação a Objetos

--- javascript-oo/Cliente.js
+++ javascript-oo/Cliente.js
@@ -1,5 +1,14 @@
 //Aplicando abstração
 export class Cliente {
     nome;
-    cpf;
+    _cpf; //indicando que o atributo é privado
+
+    get cpf(){
+        return this._cpf;
+    }
+
+    constructor(nome,cpf){
+        this.nome = nome;
+        this._cpf = cpf;
+    }
 }
--- javascript-oo/ContaCorrente.js
+++ javascript-oo/ContaCorrente.js
@@ -1,10 +1,17 @@
 import { Cliente } from "./Cliente.js";
 
 export class ContaCorrente{
+    static numeroDeContas=0;
     agencia;
-
     _cliente; // transformando o cliente em um atributo privado
     
+    // Aplicando encapsulamento
+     // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
+    _saldo=0; // convenção entre a comunidade js pra indicar que um atributo é privado
+
+    get saldo(){
+        return this._saldo;
+    }
     //método para atribuir um cliente
     set cliente(novoValor){
         if(novoValor instanceof Cliente){
@@ -15,12 +22,11 @@ export class ContaCorrente{
     get cliente(){
         return this._cliente;
     }
-    // Aplicando encapsulamento
-     // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
-    _saldo=0; // convenção entre a comunidade js pra indicar que um atributo é privado
 
-    get saldo(){
-        return this._saldo;
+    constructor(agencia,cliente){
+        this.agencia = agencia;
+        this.cliente = cliente;
+        ContaCorrente.numeroDeContas +=1;
     }
 
     sacar(valor){ //método que recebe um parâmetro
