eaf93108f39190f49214d97344bfe3be4e23e8fd
Lorena Nascimento <lorenanascimento28@gmail.com>
2022-01-06 20:30:50 -0300

    Aula 1 - Curso JavaScript: primeiros passos

---
+++ javascript-primeirospassos/1-meuPrimeiroPrograma.js
@@ -0,0 +1,11 @@
+console.log("Meu Primeiro Programa JS: trabalhando com varáveis!");
+
+const idade = 26;
+console.log(idade);
+
+const idadeSomada = idade + 3;
+console.log(idadeSomada)
+console.log(idade+2);
+console.log(idade-2);
+console.log(idade/2);
+console.log(idade*2);
