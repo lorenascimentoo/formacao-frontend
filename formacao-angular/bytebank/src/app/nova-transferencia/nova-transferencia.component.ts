36daf709f411266d0949faa5575d2d285e6af673
Lorena Nascimento <lorenanascimento28@gmail.com>
2022-03-11 16:16:11 -0300

    Aula 3 - Angular: comecando com o framework

--- formacao-angular/bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
+++ formacao-angular/bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 @Component({
   selector: 'app-nova-transferencia',
@@ -7,6 +7,10 @@ import { Component } from "@angular/core";
 })
 
 export class NovaTransferenciaComponent{
+
+  @Output() aoTranferir = new EventEmitter<any>();
+
+
   valor:number;
   destino:number;
 
@@ -14,5 +18,14 @@ export class NovaTransferenciaComponent{
     console.log('Solicitada nova transferencia');
     console.log("Valor: ",this.valor);
     console.log("Destino: ",this.destino);
+
+    this.aoTranferir.emit({valor: this.valor, destino:this.destino});
+
+    this.limparDados();
+  }
+
+  limparDados(){
+    this.valor = 0;
+    this.destino = 0;
   }
 }
